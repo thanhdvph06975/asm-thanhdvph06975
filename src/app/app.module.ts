c4f2490c99eec1ba6dbe39b09669daaa8107a228
thanhdvph06975 <demonstyle30@gmail.com>
2020-03-26 18:03:55 +0700

    8 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { CategoryComponent } from './category/category.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, MenuComponent, CategoryComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, MenuComponent, CategoryComponent, ProductListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
